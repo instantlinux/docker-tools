8cce971f6752949020354ae8d53d18ccf17b6909
Rich Braun <richb@pioneer.ci.net>
2018-10-08 17:16:17 -0700

    SYS-342 finalize nagios updates

--- images/nagiosql/entrypoint.sh
+++ images/nagiosql/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -xe
 
 PATH=$PATH:/opt/nagios/bin
-NAGIOS_DIR=/opt/nagios/etc/objects
+NAGIOS_ETC=/opt/nagios/etc
+NAGIOS_OBJ=$NAGIOS_ETC/objects
 
 if [ ! -f /etc/timezone ] && [ ! -z "$TZ" ]; then
   # At first startup, set timezone
@@ -19,32 +20,32 @@ sed -e "s/{{ DB_HOST }}/$DB_HOST/" \
     -e "s/{{ DB_PORT }}/$DB_PORT/" \
     -e "s/{{ DB_USER }}/$DB_USER/" \
     /var/www/nagiosql/config/settings.php.j2 > /var/www/nagiosql/config/settings.php
-sed -i -e "s:use_timezone=UTC:use_timezone=$TZ:" /opt/nagios/etc/nagios.cfg
+sed -i -e "s:use_timezone=UTC:use_timezone=$TZ:" $NAGIOS_ETC/nagios.cfg
 
 for dir in backup/hosts backup/services hosts services; do
-  mkdir -p $NAGIOS_DIR/$dir
+  mkdir -p $NAGIOS_OBJ/$dir
 done
-[ -f /etc/nagiosql] || ln -s $NAGIOS_DIR /etc/nagiosql
-[ -f /usr/local/nagios ] || ln -s /opt/nagios /usr/local
+[ -e /etc/nagiosql ] || ln -s $NAGIOS_OBJ /etc/nagiosql
+[ -e /usr/local/nagios ] || ln -s /opt/nagios /usr/local
 for file in contactgroups contacttemplates hostdependencies hostescalations \
       hostextinfo hostgroups hosttemplates servicedependencies \
       serviceescalations serviceextinfo servicetemplates timeperiods; do
-  if ! grep -q $file.cfg /opt/nagios/etc/nagios.cfg; then
-    echo cfg_file=$NAGIOS_DIR/$file.cfg >> /opt/nagios/etc/nagios.cfg
+  if ! grep -q $file.cfg $NAGIOS_ETC/nagios.cfg; then
+    echo cfg_file=$NAGIOS_OBJ/$file.cfg >> $NAGIOS_ETC/nagios.cfg
   fi
 done
 for dir in hosts services; do
-  if ! grep -q $NAGIOS_DIR/$dir /opt/nagios/etc/nagios.cfg; then
-    echo cfg_dir=$NAGIOS_DIR/$dir >> /opt/nagios/etc/nagios.cfg
+  if ! grep -q $NAGIOS_OBJ/$dir $NAGIOS_ETC/nagios.cfg; then
+    echo cfg_dir=$NAGIOS_OBJ/$dir >> $NAGIOS_ETC/nagios.cfg
   fi
 done
-if ! grep -q '^$USER2' /opt/nagios/etc/resource.cfg; then
-  echo '$USER2$=/opt/nagios/plugins' >> /opt/nagios/etc/resource.cfg
+if ! grep -q '^$USER2' $NAGIOS_ETC/resource.cfg; then
+  echo '$USER2$=/opt/nagios/plugins' >> $NAGIOS_ETC/resource.cfg
 fi
-echo '# Removed to avoid conflict with NagiosQL' > $NAGIOS_DIR/localhost.cfg
-echo '# Removed to avoid conflict with NagiosQL' > $NAGIOS_DIR/templates.cfg
+echo '# Removed to avoid conflict with NagiosQL' > $NAGIOS_OBJ/localhost.cfg
+echo '# Removed to avoid conflict with NagiosQL' > $NAGIOS_OBJ/templates.cfg
 
-chown -R $APACHE_USER $NAGIOS_DIR \
+chown -R $APACHE_USER $NAGIOS_OBJ \
   /var/www/nagiosql/config/settings.php
 
 . /etc/apache2/envvars
