affd3a2c4a26e51d8852d9572d0e24789ae07461
Rich Braun <richb@pioneer.ci.net>
2019-01-19 18:13:05 -0800

    SYS-373 add VENDORID parameter and /etc/nut/local volume support, nut-upsd issue #13

--- images/nut-upsd/README.md
+++ images/nut-upsd/README.md
@@ -17,7 +17,9 @@ This will expose TCP port 3493; to reach it with the standard Nagios plugin, set
 
 As a read-only service intended for monitoring, this container makes no attempt to lock down network security.
 
-Verified with the most-common type of UPS, the APC consumer-grade product. Note that the usbhid-ups driver for APC requires you to provide the correct 12-digit hardware serial number. All other parameter defaults will work.
+Verified with the most-common type of UPS, the APC consumer-grade product; Tripp Lite models also (probably) work. Note that the usbhid-ups driver for APC requires you to provide the correct 12-digit hardware serial number. All other parameter defaults will work.
+
+If you have a different model of UPS, contents of the files ups.conf, upsd.conf, upsmon.conf, and/or upsd.users can be overridden by mounting them to /etc/nut/local.
 
 If you have more than one UPS connected to a host, run more than one copy of this container and bind the container port 3493 from each to a separate TCP port.
 
@@ -41,6 +43,7 @@ SECRET | nut-upsd-password | secret to use for API user
 SERIAL | | hardware serial number of UPS
 SERVER | master | master or slave priority for scripts
 USER | nut | local user
+VENDORID | | vendor ID for ups.conf
 
 ### Secrets
 
--- images/nut-upsd/entrypoint.sh
+++ images/nut-upsd/entrypoint.sh
@@ -3,25 +3,44 @@
 API_PASSWORD=$(cat /run/secrets/$SECRET)
 
 if [ ! -e /etc/nut/.setup ]; then
-  cat <<EOF >>/etc/nut/ups.conf
+  if [ -e /etc/nut/local/ups.conf ]; then
+    cp /etc/nut/local/ups.conf /etc/nut/ups.conf
+  else
+    cat <<EOF >>/etc/nut/ups.conf
 [$NAME]
         driver = $DRIVER
         port = $PORT
         serial = "$SERIAL"
         desc = "$DESCRIPTION"
 EOF
-  cat <<EOF >>/etc/nut/upsd.conf
+    if [ ! -z "$VENDORID" ]; then
+      echo "        vendorid = $VENDORID" >> /etc/nut/ups.conf
+    fi
+  fi
+  if [ -e /etc/nut/local/ups.conf ]; then
+    cp /etc/nut/local/ups.conf /etc/nut/ups.conf
+  else
+    cat <<EOF >>/etc/nut/upsd.conf
 LISTEN 0.0.0.0
 EOF
-  cat <<EOF >>/etc/nut/upsd.users
+  fi
+  if [ -e /etc/nut/local/upsd.users ]; then
+    cp /etc/nut/local/upsd.users /etc/nut/upsd.users
+  else
+    cat <<EOF >>/etc/nut/upsd.users
 [$API_USER]
         password = $API_PASSWORD
         upsmon $SERVER
 EOF
-  cat <<EOF >>/etc/nut/upsmon.conf
+  fi
+  if [ -e /etc/nut/local/upsmon.conf ]; then
+    cp /etc/nut/local/upsmon.conf /etc/nut/upsmon.conf
+  else
+    cat <<EOF >>/etc/nut/upsmon.conf
 MONITOR $NAME@localhost 1 $API_USER $API_PASSWORD $SERVER
 RUN_AS_USER $USER
 EOF
+  fi
   touch /etc/nut/.setup
 else
   # restarting - eliminate flakiness noted in issue #7
@@ -34,5 +53,5 @@ chown $USER.$GROUP /dev/shm/nut
 [ -e /var/run/nut ] || ln -s /dev/shm/nut /var/run
 
 /usr/sbin/upsdrvctl -u root start
-/usr/sbin/upsd -u nut
+/usr/sbin/upsd -u $USER
 exec /usr/sbin/upsmon -D
