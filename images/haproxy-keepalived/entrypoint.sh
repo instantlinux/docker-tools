d45d883fccc340d0b1c131c399e4872eec0cf9b6
Rich Braun <richb@instantlinux.net>
2021-02-28 13:18:08 -0800

    SYS-528 update k8s/Makefile.versions, deal with a few breaking-change updates

--- images/haproxy-keepalived/entrypoint.sh
+++ images/haproxy-keepalived/entrypoint.sh
@@ -44,11 +44,16 @@ if [ -d /etc/haproxy.d ] && [ "$(ls -A /etc/haproxy.d)" ]; then
   CMD_OPTS="-- /etc/haproxy.d/*"
 fi
 
-sed -i -e 's/^$ModLoad imklog/#\$ModLoad imklog/' \
-       -e 's/#$ModLoad imudp/\$ModLoad imudp/' \
-       -e 's/^$ModLoad immark/#\$ModLoad immark/' \
-       -e 's/#$UDPServerRun 514/\$UDPServerRun 514/' /etc/rsyslog.conf
-sed -i -e '/$UDPServerRun 514/a $UDPServerAddress 127.0.0.1' /etc/rsyslog.conf
+sed -i -e 's/^module[(]load="imklog"/# module(load="imklog"/' \
+       -e 's/^module[(]load="immark"/# module(load="immark"/' /etc/rsyslog.conf
+if [ ! -e /etc/rsyslog.d/udp.conf ]; then
+  mkdir -p /etc/rsyslog.d
+  cat <<EOF > /etc/rsyslog.d/udp.conf
+module(load="imudp")
+\$UDPServerRun 514
+\$UDPServerAddress 127.0.0.1
+EOF
+fi
 > /var/log/messages
 rm -f /run/rsyslogd.pid && rsyslogd
 
