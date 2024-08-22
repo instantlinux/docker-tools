f363684abccdb6a9446171a6e800e7c931e8dd24
Rich Braun <richb@instantlinux.net>
2024-08-22 07:41:01 -0700

    SYS-623 re-implement weewx installation for version 5.x

--- images/weewx/entrypoint.sh
+++ images/weewx/entrypoint.sh
@@ -6,24 +6,27 @@ WUNDER_API_KEY=$(cat /run/secrets/weewx-wunderground-apikey)
 SSHKEY=weewx-rsync-sshkey
 WX_GROUP=dialout
 HOMEDIR=/home/$WX_USER
-PATH=$HOMEDIR/bin:$PATH
+PATH=$HOMEDIR/weewx-venv/bin:$PATH
 
 if [ ! -f /etc/timezone ] && [ ! -z "$TZ" ]; then
   # At first startup, set timezone
-  cp /usr/share/zoneinfo/$TZ /etc/localtime
+  cat /usr/share/zoneinfo/$TZ > /etc/localtime
   echo $TZ >/etc/timezone
 fi
 
-if [ ! -e $HOMEDIR/weewx.conf.bak ]; then
+if [ ! -e $WX_ROOT/weewx.conf.bak ]; then
   if [ -z ${STATION_URL/ /} ]; then
     REGISTER_OPT=false
   else
     REGISTER_OPT=true
   fi
   # Edit distributed weewx.conf settings after adding driver for STATION_TYPE
-  sed -i -e "s+-/var/log/messages+$SYSLOG_DEST+" /etc/rsyslog.conf
-  wee_config --reconfigure --driver=weewx.drivers.$(
-    echo $STATION_TYPE | tr [:upper:] [:lower:]) --no-prompt
+  cp /etc/rsyslog.conf /tmp
+  sed -e "s+-/var/log/messages+$SYSLOG_DEST+" \
+      -e 's/^.*imklog/# disabled ("imklog/' \
+      /tmp/rsyslog.conf >/etc/rsyslog.conf
+  su $WX_USER -c "weectl station reconfigure --driver=weewx.drivers.$(
+    echo $STATION_TYPE | tr [:upper:] [:lower:]) --no-prompt"
   sed --in-place=.bak -e "s/location = DESC/location = \"$LOCATION\"/" \
   -e "s/altitude = 0, foot/altitude = $ALTITUDE/" \
   -e "s/archive_interval =.*/archive_interval = $LOGGING_INTERVAL/" \
@@ -39,7 +42,6 @@ if [ ! -e $HOMEDIR/weewx.conf.bak ]; then
   -e "s/ station =.*/ station = $STATION_ID/" \
   -e "s+#station_url =.*+station_url = $STATION_URL+" \
   -e "s/password = replace_me$/password = $WUNDER_PASS  # Und/" \
-  -e "s/location = \"INSERT_LOCATION_HERE /location = \"$XTIDE_LOCATION\"  # \"/" \
   -e "s/lid =/#lid =/" \
   -e "s/foid =/#foid =/" \
   -e "s/api_key = INSERT_WU_API_KEY_HERE/api_key = $WUNDER_API_KEY/" \
@@ -53,8 +55,8 @@ if [ ! -e $HOMEDIR/weewx.conf.bak ]; then
   -e "s/server = replace_me/server = $RSYNC_HOST/" \
   -e "s/user = replace_me/user = $RSYNC_USER/" \
   -e "s:path = replace_me:path = $RSYNC_DEST:" \
-  $HOMEDIR/weewx.conf
-  if [ -e $HOMEDIR/skins/WeeGreen/skin.conf ]; then
+  $WX_ROOT/weewx.conf
+  if [ -e $WX_ROOT/skins/WeeGreen/skin.conf ]; then
     sed -i -e "s/ID=[A-Z0-9]$/ID=$STATION_ID/" \
       -e "s/computerType =.*/computerType = $COMPUTER_TYPE/" \
       -e "s/modelNumber =.*/modelNumber = $STATION_MODEL/" \
@@ -62,34 +64,34 @@ if [ ! -e $HOMEDIR/weewx.conf.bak ]; then
       -e "s/[#]*optionalAccessories = .*/optionalAccessories = $OPTIONAL_ACCESSORIES/" \
       -e "s/siteOperator =.*/siteOperator = $OPERATOR/" \
       -e "s+webcamLink = .*+webcamLink = $WEBCAM_URL+" \
-      $HOMEDIR/skins/WeeGreen/skin.conf
+      $WX_ROOT/skins/WeeGreen/skin.conf
   fi
   if [ "$AIRLINK_HOST" != "" ]; then
     sed -i -e "s/hostname = airlink$/hostname = $AIRLINK_HOST/" \
-      $HOMEDIR/weewx.conf
+      $WX_ROOT/weewx.conf
   else
-    sed -i "/[[Sensor1]]/{n;s/.*/        enable = false/}" $HOMEDIR/weewx.conf
+    sed -i "/[[Sensor1]]/{n;s/.*/        enable = false/}" $WX_ROOT/weewx.conf
   fi
   if [ $SKIN != Season ]; then
     sed -i \
-      "/skin = Season/,/enable = true/c skin = Seasons\n enable = false" $HOMEDIR/weewx.conf
+      "/skin = Season/,/enable = true/c skin = Seasons\n enable = false" $WX_ROOT/weewx.conf
     sed -i \
-      "/skin = Standard/,/enable = false/c skin = $SKIN\n enable = true" $HOMEDIR/weewx.conf
+      "/skin = Standard/,/enable = false/c skin = $SKIN\n enable = true" $WX_ROOT/weewx.conf
   fi
 
   # Change the 5th "enable = false" which is Wunderground StdRESTful toggle
   awk '/enable = false/{c++;if(c==5){sub("enable = false","enable = true");c=0}}1' \
-    $HOMEDIR/weewx.conf > $HOMEDIR/weewx.conf.new
-  mv $HOMEDIR/weewx.conf $HOMEDIR/weewx.conf.awk
-  mv $HOMEDIR/weewx.conf.new $HOMEDIR/weewx.conf
+    $WX_ROOT/weewx.conf > $WX_ROOT/weewx.conf.new
+  mv $WX_ROOT/weewx.conf $WX_ROOT/weewx.conf.awk
+  mv $WX_ROOT/weewx.conf.new $WX_ROOT/weewx.conf
 
   # At first startup, set configs from environment
-  wee_device --set-interval=$(( $LOGGING_INTERVAL / 60 ))
-  wee_device --set-rain-year-start=$RAIN_YEAR_START
-  wee_device --set-tz-code=$TZ_CODE
+  su $WX_USER -c \
+  "weectl device --set-interval=$(( $LOGGING_INTERVAL / 60 )) &&
+   weectl device --set-rain-year-start=$RAIN_YEAR_START &&
+   weectl device --set-tz-code=$TZ_CODE"
 fi
 
-sed -i -e 's/^.*imklog/# disabled ("imklog/' /etc/rsyslog.conf
 TZ=UTC rsyslogd
 
 cp /run/secrets/$SSHKEY /run/$SSHKEY && chmod 400 /run/$SSHKEY
@@ -114,11 +116,11 @@ EOF
   cat /tmp/sshkey >> $HOMEDIR/.ssh/known_hosts && rm /tmp/sshkey
 fi
 
-chown -R $WX_USER $HOMEDIR $HTML_ROOT /run/$SSHKEY
+chown -R $WX_USER $HTML_ROOT /run/$SSHKEY
 # Docker 18.02 changes group to root at launch
-chgrp $WX_GROUP /dev/ttyUSB0
+#chgrp $WX_GROUP /dev/ttyUSB0
 set +e
-su $WX_USER -c "PATH=$PATH weewxd $HOMEDIR/weewx.conf|grep -v LOOP:"
+su $WX_USER -c "PATH=$PATH weewxd|grep -v LOOP:"
 # Failure: attempt restart only every 2 minutes
 sleep 120
 exit 1
