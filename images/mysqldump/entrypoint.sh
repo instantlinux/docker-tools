dfcf8fa717c5dac4aa8d3f52f56a96e965ead640
Rich Braun <richb@instantlinux.net>
2026-07-14 20:34:20 -0700

    SYS-695 add zstd support (for compression) to mysqldump image (#286)

--- images/mysqldump/entrypoint.sh
+++ images/mysqldump/entrypoint.sh
@@ -11,10 +11,20 @@ echo [client] > $CNF
 cat /run/secrets/$DB_CREDS_SECRETNAME >> $CNF
 [ $SKIP_SSL = true ] && echo "skip-ssl=true" >> $CNF
 
+[ -z "$COMPRESS" ] && COMPRESS=bzip2
+
+cat <<EOF >/home/$USERNAME/.profile
+export COMPRESS="$COMPRESS"
+export COMPRESSLEVEL=$COMPRESSLEVEL
+export COMPRESSOPTS="$COMPRESSOPTS"
+export SKEW_SECONDS=$SKEW_SECONDS
+EOF
+
 touch $LOG
-chown $USERNAME /var/backup $LOG $CNF
+chown $USERNAME /var/backup $LOG $CNF /home/$USERNAME/.profile
 chmod 400 $CNF
-echo "$MINUTE $HOUR * * *   /usr/local/bin/mysql-backup.sh $KEEP_DAYS $SERVERS" \
+echo "$MINUTE $HOUR * * *   . /home/$USERNAME/.profile && \
+   /usr/local/bin/mysql-backup.sh $KEEP_DAYS $SERVERS" \
    | crontab -u $USERNAME -
 
 crond
